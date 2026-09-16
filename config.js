7981f01cdb0c98dc270b934e58557fb9f9fd0b36
upskillafricafoundation <chrisckindness@gmail.com>
2026-09-16 20:45:09 -0200

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
    else in the frontend should hardcode it.
    ========================================================= */
 window.UAF_CONFIG = {
-   API_URL: "https://script.google.com/macros/s/AKfycbxmd3eYHQ2heZA7Q-lEkBARNzBRGHGuHBHqhJvu_uPlMEFINPbk37TfWbB_o1tM3_YJ/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbxmd3eYHQ2heZA7Q-lEkBARNzBRGHGuHBHqhJvu_uPlMEFINPbk37TfWbB_o1tM3_YJ/exec"
 };
