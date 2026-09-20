51a7a26ab453b94aa5786b6761be9f640aa4986a
upskillafricafoundation <chrisckindness@gmail.com>
2026-09-20 00:33:37 -0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
    icons, media assets) for full offline execution and auto-sync.
    ========================================================= */
 
-const CACHE_VERSION = "uaf-impact-shell-v13";
+const CACHE_VERSION = "uaf-impact-shell-v14";
 const APP_SHELL = [
   "./",
   "./index.html",
